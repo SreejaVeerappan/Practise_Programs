a5aaef3b2a1bcfb260173e88ba463384f29e9b1f
SreejaVeerappan <144646423+SreejaVeerappan@users.noreply.github.com>
2024-11-05 20:47:13 +0530

    wordcount

---
+++ CountWords.java
@@ -0,0 +1,12 @@
+package basic.training.com;
+
+public class CountWords {
+
+	public static void main(String[] args) {
+		String word="Hello everyone";
+		String[] words=word.split(" ");
+		System.out.println(words.length);
+
+	}
+
+}
