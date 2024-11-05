99ceac73d593ae1eb11e2f92c6b0aa2affa72f11
SreejaVeerappan <144646423+SreejaVeerappan@users.noreply.github.com>
2024-11-05 18:33:28 +0530

    upper and lowercase

---
+++ StringUpperandlowercase.java
@@ -0,0 +1,13 @@
+package basic.training.com;
+
+public class StringUpperandlowercase {
+
+	public static void main(String[] args) {
+	String str="Python";
+	String lcase=str.toLowerCase();
+	String ucase=str.toUpperCase();
+	System.out.printf("Upprecase:"+ucase + " "+ "Lowercase:"+lcase);
+	
+	}
+
+}
