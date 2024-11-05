ceec2a23c16cdb489e0c0a0c1535cbbcffd16e4b
SreejaVeerappan <144646423+SreejaVeerappan@users.noreply.github.com>
2024-11-05 18:41:17 +0530

    length of String

---
+++ Stringlength.java
@@ -0,0 +1,11 @@
+package basic.training.com;
+
+public class Stringlength {
+
+	public static void main(String[] args) {
+		String le="hello";
+		System.out.println("length of the String hello :" + le.length());
+
+	}
+
+}
