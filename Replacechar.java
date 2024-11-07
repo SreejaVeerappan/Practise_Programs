fefc5e3897f2fa7bb292801ab5caed9b08d764c0
SreejaVeerappan <144646423+SreejaVeerappan@users.noreply.github.com>
2024-11-07 22:20:58 +0530

    ReplaceChar

---
+++ Replacechar.java
@@ -0,0 +1,24 @@
+package basic.training.com;
+
+public class Replacechar {
+
+	public static void main(String[] args) {
+//		String word="bana";
+//		char ch;
+//		char a='a';
+//		for(int i=0;i<word.length();i++) {
+//			ch=word.charAt(i);
+//			if(i==a) {
+//			String nw=word.replace("a", "b");	
+//			System.out.println(nw);
+//		
+//			}
+//			
+//		}
+
+		String word="Banana";
+		String nw=word.replace('a', 'o');
+		System.out.println(nw);
+	}
+
+}
